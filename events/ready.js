cfb390eb8f65a94f9559c2d9afc55143b40d8d85
Glitch (davet-yoneticisi) <none>
2020-02-15 15:01:06 +0000

    👜🎇 Checkpoint ./events/ready.js:10732941/79

--- events/ready.js
+++ events/ready.js
@@ -14,7 +14,7 @@ module.exports = async client => {
   
   console.log(`BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("oline");
-  client.user.setGame(`🔥 Beni Sunucuya Eklemek İçin !davet`);
+  client.user.setGame(`🔥!yardım🔥!davet🔥'client.guilds.size' `);
   console.log(`BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
   require("../util/dashboard.js")(client);
 };
